ca0df4efdf90c74b931ee46f38c186871687a007
baguswijaksono <baguswijaksono292@gmail.com>
2023-12-15 04:52:31 +0700

    add url redirection function

--- index.php
+++ index.php
@@ -1,10 +1,34 @@
 <?php
-    $http_response_code = 200; 
 
-    http_response_code($http_response_code); 
-    $response = array(
-        "code" => $http_response_code,
-        "message" => "Welcome to the API"
-    );
+require_once('db.php');
+
+if (isset($_GET['short_url'])) {
+    $short_url = $_GET['short_url'];
+
+    // Prepare an SQL statement to retrieve the original URL
+    $stmt = $conn->prepare("SELECT orurl FROM urls WHERE shurl = ?");
+
+    // Bind parameters to prevent SQL injection
+    $stmt->bind_param("s", $short_url);
+
+    // Execute the statement
+    $stmt->execute();
+    $result = $stmt->get_result();
+
+    if ($result->num_rows > 0) {
+        $row = $result->fetch_assoc();
+        $original_url = $row['orurl'];
+        // Redirect to the original URL
+        header("Location: $original_url");
+        exit();
+    } else {
+        http_response_code(404); // Not Found
+        $response = array("error" => "Short URL not found");
+        echo json_encode($response);
+    }
+} else {
+    http_response_code(400);
+    $response = array("error" => "Short URL not provided");
     echo json_encode($response);
+}
 ?>
